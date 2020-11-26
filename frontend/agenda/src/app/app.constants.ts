3e41e312ef500946b8ee9536d0b232482ca0994f
Yuri Strack <yuri.strack@gmail.com>
2020-11-25 22:47:05 -0300

    Deploy do front no heroku

--- frontend/agenda/src/app/app.constants.ts
+++ frontend/agenda/src/app/app.constants.ts
@@ -1,6 +1,6 @@
 import { environment } from 'src/environments/environment';
 
-const PROD_BASE_URL = '';
+const PROD_BASE_URL = 'https://progweb-agenda-backend.herokuapp.com';
 const LOCAL_BASE_URL = 'http://127.0.0.1:8000';
 
 export let BASE_URL: string;
