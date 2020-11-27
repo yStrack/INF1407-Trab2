787bd4668947289ab7a530477a78c53f3f99a582
Emanuel Umbelino <umbelino.emanuel@gmail.com>
2020-11-27 15:28:13 -0300

    Validacoes para criar eventos

--- frontend/agenda/src/app/components/dialog-create-event/dialog-create-event.component.ts
+++ frontend/agenda/src/app/components/dialog-create-event/dialog-create-event.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MessageService } from 'src/app/services/message.service';
 
 interface Event {
   title: string;
@@ -16,27 +18,78 @@ export class DialogCreateEventComponent implements OnInit {
 
   editing = false;
 
-  newEvent: Event = {
-    title: '',
-    start: '',
-    end: '',
-    allDay: true
-  };
+  newEvent = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    start: new FormControl('', [Validators.required]),
+    end: new FormControl('', []),
+  });
 
   constructor(
     public dialogRef: MatDialogRef<DialogCreateEventComponent>,
-    @Inject(MAT_DIALOG_DATA) public event: Event
+    @Inject(MAT_DIALOG_DATA) public event: Event,
+    private message: MessageService,
   ) { }
 
   ngOnInit(): void {
-    this.newEvent.title = this.event.title;
-    this.newEvent.start = this.event.start;
-    this.newEvent.end = this.event.end;
+    this.newEvent.setValue({
+      title: this.event.title ? this.event.title : '',
+      start: this.event.start ? this.event.start : '',
+      end: this.event.end ? this.event.end : '',
+    });
     this.editing = this.event.title !== undefined;
     // console.log(this.newEvent);
+
+    const s = this.newEvent.get('start');
+    if (s) {
+      s.valueChanges.subscribe(val => {
+        this.dateValidator(this.newEvent);
+      });
+    }
+    const e = this.newEvent.get('end');
+    if (e) {
+      e.valueChanges.subscribe(val => {
+        this.dateValidator(this.newEvent);
+      });
+    }
+  }
+
+  dateValidator(g: FormGroup): void {
+    const start = new Date(g.get('start')?.value);
+    const endV = g.get('end');
+    const end = new Date(endV?.value);
+    if (start > end) {
+      endV?.setErrors({ date: true });
+    } else {
+      endV?.setErrors(null);
+    }
+  }
+
+  getErrorMessage(formGroup: FormGroup, formControlName: string): string {
+    const form = formGroup.controls[formControlName];
+    if (form.hasError('required')) {
+      return 'Campo obrigatório';
+    } else if (form.hasError('date')) {
+      return 'Data de fim precisa ser menor que data de início';
+    } else {
+      return '';
+    }
   }
 
   close(): void {
     this.dialogRef.close();
+  }
+
+  save(): void {
+    if (this.newEvent.valid) {
+      const newEvent: Event = {
+        title: this.newEvent.value.title,
+        start: this.newEvent.value.start,
+        end: this.newEvent.value.end,
+        allDay: true
+      };
+      this.dialogRef.close(newEvent);
+    } else {
+      this.message.show('Conserte os erros', 'fechar', 'warn');
+    }
   }
 }
